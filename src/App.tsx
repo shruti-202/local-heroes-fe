822f9a09d715a9670947cd52d4321de3394bc47f
Shruti <shrutigawande1910@gmail.com>
2023-09-22 21:27:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Home from "./pages/Home"
+import { Routes,Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Profile from "./pages/Profile"
+import Hero from "./pages/Hero"
+import MainLayout from "./layout/MainLayout"
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout/>}>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/hero/:heroId" element={<Hero/>}/>
+      </Route>
+      
+    </Routes>
+  )
+}
+
+export default App
+
+
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/organisms/Header";
+import Footer from "../components/organisms/Footer";
+const MainLayout = () => {
+  return (
+      <main>
+        <Header/>
+        <Outlet />
+        <Footer/>
+      </main>
+  );
+};
+
+export default MainLayout;
