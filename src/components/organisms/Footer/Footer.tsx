f3ab65cc250063a682761d427ba4cc28cb44ce8f
Shruti <shrutigawande1910@gmail.com>
2024-04-30 00:22:40 +0530

    feat: Footer

---
+++ src/components/organisms/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p className="footer-text">
+          Made with 💚 by <a href="https://shrutis-portfolio.netlify.app/" target="_blank"><u><i>Shruti Gawande</i></u> in IN</a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/organisms/Header";
+import Footer from "../components/organisms/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <main>
       <Header />
       <Outlet />
+      <Footer/>
     </main>
   );
 };
